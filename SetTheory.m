e3f6b286d5fb425bf3a2d2563de59eb1f255465e
Paco Jain <pacojain@gmail.com>
2014-12-16 14:47:16 -0600

    Set up as an eclipse project and added boilerplate SetTheory.m file

---
+++ SetTheory.m
@@ -0,0 +1,15 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Nov Dec 16, 2014 
+   Author: Paco Jain (pacojain@gmail.com)
+*)
+
+BeginPackage["SetTheory`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+End[]
+
+EndPackage[]
